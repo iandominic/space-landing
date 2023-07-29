c7074ff02ecd9a7acdeef867b155d0068c976ae6
iandominic <javalavama@gmail.com>
2023-07-30 01:23:41 +0800

    home-anim

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/hero",
+        permanent: true,
+      },
+    ];
+  },
+};
 
 module.exports = nextConfig;
