8d87a267dd0b5830ee9a3bb220208fd9ccf2eb85
iandominic <javalavama@gmail.com>
2023-07-30 01:19:38 +0800

    home-anim

--- src/app/index.js
+++ src/app/index.js
@@ -1,5 +1,10 @@
 import React from "react";
 
+export const metadata = {
+  title: "Space Landing",
+  description: "From: Frontend Mentor",
+};
+
 const Home = () => {
   return <div>page</div>;
 };
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,11 +1,6 @@
 import Header from "./components/Header";
 import "./globals.css";
 
-export const metadata = {
-  title: "Space Landing",
-  description: "From: Frontend Mentor",
-};
-
 const RootLayout = ({ children }) => {
   return (
     <html lang="en">
