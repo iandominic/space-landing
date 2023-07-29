7a872f2eec3b086bf72b97b4859e9e99fb07565b
iandominic <javalavama@gmail.com>
2023-07-30 03:47:01 +0800

    destination-test

--- src/app/destination/page.jsx
+++ src/app/destination/page.jsx
@@ -1,13 +1,23 @@
 "use client";
 
 import Image from "next/image";
-import React, { useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import moon from "../../assets/planets/image-moon.webp";
+import mars from "../../assets/planets/image-mars.webp";
+import europa from "../../assets/planets/image-europa.webp";
+import titan from "../../assets/planets/image-titan.webp";
 import "../components/styles/styles.css";
 import { Divider } from "antd";
-import { useInView, motion } from "framer-motion";
+import { useInView, motion, useAnimation } from "framer-motion";
 
 const Planets = () => {
+  const [desti, setDesti] = useState(0);
+  const [animateDiv, setAnimateDiv] = useState();
+
+  useEffect(() => {
+    setAnimateDiv(true);
+  }, []);
+
   const destination = [
     {
       img: moon,
@@ -23,25 +33,41 @@ const Planets = () => {
       travel: "3 days",
     },
     {
-      img: moon,
+      img: mars,
       title: "Mars",
-      desc: " See our planet as you’ve never seen it before. A perfect relaxing trip away to help regain perspective and come back refreshed. While you’re there, take in some history by visiting the Luna 2 and Apollo 11 landing sites.",
-      distance: "384,400 km",
-      travel: "3 days",
+      desc: [
+        "Don’t forget to pack your hiking boots. You’ll need",
+        "them to tackle Olympus Mons, the tallest planetary",
+        "mountain in our solar system. It’s two and a half",
+        "times the size of Everest!",
+      ],
+      distance: "225 MIL. km",
+      travel: "9 months",
     },
     {
-      img: moon,
+      img: europa,
       title: "Europa",
-      desc: " See our planet as you’ve never seen it before. A perfect relaxing trip away to help regain perspective and come back refreshed. While you’re there, take in some history by visiting the Luna 2 and Apollo 11 landing sites.",
-      distance: "384,400 km",
-      travel: "3 days",
+      desc: [
+        "The smallest of the four Galilean moons orbiting",
+        "Jupiter, Europa is a winter lover’s dream. With an",
+        "icy surface, it’s perfect for a bit of ice skating,",
+        "curling, hockey, or simple relaxation in your snug",
+        "wintery cabin.",
+      ],
+      distance: "628 MIL. km",
+      travel: "3 years",
     },
     {
-      img: moon,
+      img: titan,
       title: "Titan",
-      desc: " See our planet as you’ve never seen it before. A perfect relaxing trip away to help regain perspective and come back refreshed. While you’re there, take in some history by visiting the Luna 2 and Apollo 11 landing sites.",
-      distance: "384,400 km",
-      travel: "3 days",
+      desc: [
+        "The only moon known to have a dense atmosphere",
+        "other than Earth, Titan is a home away from home",
+        "(just a few hundred degrees colder!). As a bonus,",
+        "you get striking views of the Rings of Saturn.",
+      ],
+      distance: "1.6 BIL. km",
+      travel: "7 years",
     },
   ];
 
@@ -49,22 +75,38 @@ const Planets = () => {
     {
       title: "moon",
       selected: true,
-      onClick: () => console.log("asd"),
+      onClick: () => {
+        setDesti(0);
+        setAnimateDiv(false);
+        setTimeout(() => setAnimateDiv(true), 100);
+      },
     },
     {
       title: "mars",
       selected: false,
-      onClick: () => console.log("asd"),
+      onClick: () => {
+        setDesti(1);
+        setAnimateDiv(false);
+        setTimeout(() => setAnimateDiv(true), 100);
+      },
     },
     {
       title: "europa",
       selected: false,
-      onClick: () => console.log("asd"),
+      onClick: () => {
+        setDesti(2);
+        setAnimateDiv(false);
+        setTimeout(() => setAnimateDiv(true), 100);
+      },
     },
     {
       title: "titan",
       selected: false,
-      onClick: () => console.log("asd"),
+      onClick: () => {
+        setDesti(3);
+        setAnimateDiv(false);
+        setTimeout(() => setAnimateDiv(true), 100);
+      },
     },
   ];
 
@@ -74,6 +116,8 @@ const Planets = () => {
       y: "0%",
       transition: { duration: 0.5, delay: 0.1 * i, ease: [0.33, 1, 0.68, 1] },
     }),
+    rotateInit: { scale: 0.8, rotate: -20, opacity: 0 },
+    rotatePlanet: { scale: 1, rotate: 0, opacity: 1 },
   };
 
   const textBody = useRef(null);
@@ -97,8 +141,9 @@ const Planets = () => {
           </motion.h5>
         </div>
         <motion.div
-          initial={{ scale: 0.8, rotate: -20, opacity: 0 }}
-          whileInView={{ scale: 1, rotate: 0, opacity: 1 }}
+          variants={animate}
+          initial="rotateInit"
+          animate={animateDiv ? "rotatePlanet" : ""}
           transition={{
             type: "spring",
             stiffness: 150,
@@ -107,7 +152,7 @@ const Planets = () => {
         >
           <Image
             alt=""
-            src={destination[0].img}
+            src={destination[desti].img}
             width={170}
             height={170}
             preview={false}
@@ -118,10 +163,9 @@ const Planets = () => {
             return (
               <div className="lineMask" key={index}>
                 <motion.h5
-                  className={`barlow-condensed text-[14px] font-[400] uppercase tracking-[2.36px] border-b-2 ${
-                    phrase.selected
-                      ? "text-[white] border-white py-2"
-                      : "text-[#D0D6F9] border-transparent py-2"
+                  onClick={phrase.onClick}
+                  className={`barlow-condensed text-[14px] font-[400] text-[#D0D6F9] uppercase tracking-[2.36px] slide-nav ${
+                    index === desti ? "slide-active text-white" : ""
                   }`}
                   custom={index}
                   variants={animate}
@@ -139,14 +183,14 @@ const Planets = () => {
             <motion.h3
               variants={animate}
               initial="initial"
-              animate={isInView ? "open" : ""}
+              animate={animateDiv ? "open" : ""}
               className="uppercase text-white belfair text-[56px]"
             >
-              {destination[0].title}
+              {destination[desti].title}
             </motion.h3>
           </div>
           <div className="lineMask">
-            {destination[0].desc.map((desc, index) => {
+            {destination[desti].desc.map((desc, index) => {
               return (
                 <div className="lineMask" key={index}>
                   <motion.h5
@@ -154,7 +198,7 @@ const Planets = () => {
                     custom={index}
                     variants={animate}
                     initial="initial"
-                    animate={isInView ? "open" : ""}
+                    animate={animateDiv ? "open" : ""}
                   >
                     {desc}
                   </motion.h5>
@@ -170,7 +214,7 @@ const Planets = () => {
               <motion.h5
                 variants={animate}
                 initial="initial"
-                animate={isInView ? "open" : ""}
+                animate={animateDiv ? "open" : ""}
                 className="barlow-condensed text-[14px] font-[400] text-[#D0D6F9] uppercase tracking-[2.36px]"
               >
                 Avg. distance
@@ -180,10 +224,10 @@ const Planets = () => {
               <motion.h3
                 variants={animate}
                 initial="initial"
-                animate={isInView ? "open" : ""}
+                animate={animateDiv ? "open" : ""}
                 className="uppercase text-white belfair text-[28px]"
               >
-                {destination[0].distance}
+                {destination[desti].distance}
               </motion.h3>
             </div>
           </div>
@@ -192,7 +236,7 @@ const Planets = () => {
               <motion.h5
                 variants={animate}
                 initial="initial"
-                animate={isInView ? "open" : ""}
+                animate={animateDiv ? "open" : ""}
                 className="barlow-condensed text-[14px] font-[400] text-[#D0D6F9] uppercase tracking-[2.36px]"
               >
                 est. travel time
@@ -202,10 +246,10 @@ const Planets = () => {
               <motion.h3
                 variants={animate}
                 initial="initial"
-                animate={isInView ? "open" : ""}
+                animate={animateDiv ? "open" : ""}
                 className="uppercase text-white belfair text-[28px]"
               >
-                {destination[0].travel}
+                {destination[desti].travel}
               </motion.h3>
             </div>
           </div>
